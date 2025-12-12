7331b76d9ab887a32ebc95b22e12efe3f20b7237
Nu <191132814+Thanurin@users.noreply.github.com>
2025-12-12 13:49:35 +0700

    Add files via upload

---
+++ js2.js
@@ -0,0 +1,34 @@
+document.getElementById("Khmer").addEventListener('click', function() {
+    window.location.href="https://www.elibraryofcambodia.org/";
+})
+document.getElementById("English").addEventListener('click', function() {
+    window.location.href= "https://www.elibraryofcambodia.org/?s=English"
+})
+document.getElementById("maths").addEventListener('click', function() {
+    window.location.href= "https://www.elibraryofcambodia.org/?s=%E1%9E%82%E1%9E%8E%E1%9E%B7%E1%9E%8F%E1%9E%9C%E1%9E%B7%E1%9E%91%E1%9F%92%E1%9E%99%E1%9E%B6";
+})
+document.getElementById("gpt").addEventListener('click', function() {
+    window.location.href= "https://chatgpt.com/?utm_source=google&utm_medium=paidsearch_brand&utm_campaign=GOOG_C_SEM_GBR_Core_CHT_BAU_ACQ_PER_MIX_ALL_APAC_KH_EN_042225&utm_term=gpt&utm_content=177106394774&utm_ad=747827931270&utm_match=e&gad_source=1&gad_campaignid=22472182799&gbraid=0AAAAA-IW-UVz_dDnbG8pMvZECr3UW7DPx&gclid=Cj0KCQiA9OnJBhD-ARIsAPV51xM0aP0OmJ6L4Ej61idAKO4oeaeoLURZQY2NnJaEs-a1mGb3Y5PBYWsaAmlJEALw_wcB";
+})
+document.getElementById("gemini").addEventListener('click', function() {
+    window.location.href= "https://gemini.google.com/app";
+})
+document.getElementById("Physics").addEventListener('click', function() {
+    window.location.href= "https://www.elibraryofcambodia.org/?s=%E1%9E%9A%E1%9E%BC%E1%9E%94%E1%9E%9C%E1%9E%B7%E1%9E%91%E1%9F%92%E1%9E%99%E1%9E%B6";
+})
+document.getElementById('Es').addEventListener('click', function() {
+    window.location.href= "https://www.elibraryofcambodia.org/?s=%E1%9E%95%E1%9F%82%E1%9E%93%E1%9E%8A%E1%9E%B8";
+})
+document.getElementById('Biology').addEventListener('click', function() {
+    window.location.href= "https://www.elibraryofcambodia.org/?s=%E1%9E%87%E1%9E%B8%E1%9E%9C%E1%9E%9C%E1%9E%B7%E1%9E%91%E1%9F%92%E1%9E%99%E1%9E%B6";
+})
+
+document.getElementById('history').addEventListener('click', function() {
+    window.location.href= "https://www.elibraryofcambodia.org/?s=%E1%9E%97%E1%9E%BC%E1%9E%98%E1%9E%B7%E1%9E%9C%E1%9E%B7%E1%9E%91%E1%9F%92%E1%9E%99%E1%9E%B6";
+})
+document.getElementById('Gio').addEventListener('click', function() {
+    window.location.href= "https://www.elibraryofcambodia.org/?s=%E1%9E%94%E1%9F%92%E1%9E%9A%E1%9E%9C%E1%9E%8F%E1%9F%92%E1%9E%8F%E1%9E%B7%E1%9E%9C%E1%9E%B7%E1%9E%91%E1%9F%92%E1%9E%99%E1%9E%B6";
+})
+document.getElementById('I').addEventListener('click', function() {
+    window.location.href= "https://www.elibraryofcambodia.org/?s=%E1%9E%96%E1%9E%9B%E1%9E%9A%E1%9E%8A%E1%9F%92%E1%9E%8B";
+})
